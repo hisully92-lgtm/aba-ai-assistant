7d12af6265db0178906cd0e89257282ac6086280
Heidi Sullivan <hisully92@gmail.com>
2026-06-17 07:56:02 -0400

    Fix GA - move scripts inside body not head

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,9 +109,11 @@ export default function RootLayout({
           }}
         />
       </head>
-      <head><Script async src="https://www.googletagmanager.com/gtag/js?id=G-REXJ3JT87D" /><Script id="ga" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag(String.fromCharCode(106,115),new Date());gtag(String.fromCharCode(99,111,110,102,105,103),String.fromCharCode(71,45,82,69,88,74,51,74,84,56,55,68));"}} /></head><body className={inter.className}>
+      <body><Script async src="https://www.googletagmanager.com/gtag/js?id=G-REXJ3JT87D" /><Script id="ga" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag(String.fromCharCode(106,115),new Date());gtag(String.fromCharCode(99,111,110,102,105,103),String.fromCharCode(71,45,82,69,88,74,51,74,84,56,55,68));"}} /> className={inter.className}>
         {children}
       </body>
     </html>
   );
 }
+
+
