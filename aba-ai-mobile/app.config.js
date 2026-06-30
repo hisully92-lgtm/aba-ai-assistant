1942b0f417092b30efbf946780ce8c168385b3c3
Heidi Sullivan <hisully92@gmail.com>
2026-06-30 07:23:44 -0400

    feat: separate dev client config with distinct scheme/bundle ID to avoid TestFlight URL conflict

---
+++ aba-ai-mobile/app.config.js
@@ -0,0 +1,9 @@
+﻿const baseConfig = require("./app.json");
+const devConfig = require("./app.dev.json");
+
+module.exports = () => {
+  if (process.env.EXPO_PUBLIC_USE_DEV_CONFIG === "true") {
+    return devConfig;
+  }
+  return baseConfig;
+};
